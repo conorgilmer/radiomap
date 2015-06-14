1c2431b63a2d020ff8751b324f98207ca4724b45
Conor Gilmer <conor.gilmer@gmail.com>
2015-06-14 13:48:30 +0100

    head foot

---
+++ footer.php
@@ -0,0 +1,18 @@
+
+<!--footer.php -->
+
+<div id="footer">
+        <div id="content">
+    <hr /><br/>
+    <label>&copy; No Copyright </label>
+    </div>
+</div>
+
+
+</center>
+  </body>
+</html>
+
+
+
+
---
+++ header.php
@@ -0,0 +1,14 @@
+
+<!-- header.php -->
+
+  <body>
+<center>
+<div id="header">
+        <div id="content">
+    <label>RTE LW 252 Reception</label>
+    </div>
+</div>
+
+
+
+
