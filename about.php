783e2b5529004f8e6abe13224eea49164e95b47f
Conor Gilmer <conor.gilmer@gmail.com>
2015-06-23 02:16:32 +0100

    about

---
+++ about.php
@@ -0,0 +1,34 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>RTE Radio 252 Long Wave Reception</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<?php include('header.php');  ?>
+
+<div id="body">
+        <div id="content">
+    <table align="center">
+    <tr>
+    <th colspan="2"><a href="add_data.php">Add Report</a></th>
+    <th colspan="2"><a href="list.php">List Reports</a></th>
+    <th colspan="2"><a href="index.php">Map Reports</a></th>
+    </tr>
+    <tr>
+    <th colspan="6">About</th>
+    </tr>
+    <tr>
+    <td colspan="6">On this webpage I am trying to compile a list of places where RTE Long Wave 252 can be received, and how clear it is etc.</td>
+    </tr>
+    <tr>
+    <td colspan="6">If you don't know your locations longitude and latitude, you can find it on google maps or by using <a target="_blank" href="http://www.freegeocoder.com">www.freegeocoder.com</a>.</td>
+    </tr>
+    <tr>
+    <td colspan="6">If you can't submit a report on this page, or cant obtain your longitude and latitude, you can email me (conor.gilmer@gmail.com)  your report, with your  details and I will, plot it using your town name.</td>
+    </tr>
+    </table>
+    </div>
+</div>
+<?php include('footer.php');  ?>
