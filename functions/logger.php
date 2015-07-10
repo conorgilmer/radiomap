5ab7f81a76274ebcbf221016d149e8638d427fde
Conor Gilmer <conor.gilmer@gmail.com>
2015-07-10 13:05:39 +0100

    logger

--- functions/logger.php
+++ functions/logger.php
@@ -1,19 +1,37 @@
 <?php
-// log activity to a file
+/* log activity to a file
 
-   function logToFile($filename, $type, $message)
-   { 
-   /* types / Category of Log Message
+You have to have write access to the log file you chose to use
+
+   Types / Category of Log Message
     0 - Debug
     1 - Info
     2 - Warning
     3 - Error
     4 - Crash
     */
+
+
+// [YYYY/MM/DD HH:MM:SS]	<Type>	Message
+   function basiclogToFile($filename, $type, $message)
+   { 
    // open file
    $fd = fopen($filename, "a");
    // append date/time and type to message
    $logString = "[" . date("Y/m/d h:i:s", mktime()) . "] " . "\t<" . $type .">\t" . $message; 
+   // write string
+   fwrite($fd, $logString . "\n");
+   // close file
+   fclose($fd);
+   }
+
+// [YYYY/MM/DD HH:MM:SS]	Type	<System>	Message
+   function logToFile($filename, $system, $type, $message)
+   { 
+   // open file
+   $fd = fopen($filename, "a");
+   // append date/time and type to message
+   $logString = "[" . date("Y/m/d h:i:s", mktime()) . "] " ."\t<" .$system .">\t".  $type ."\t" . $message; 
    // write string
    fwrite($fd, $logString . "\n");
    // close file
