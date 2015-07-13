deccab261728dd85d947c788ba6fb7b9b64f958d
Conor Gilmer <conor.gilmer@gmail.com>
2015-07-13 02:27:32 +0100

    cosmetic

--- list.php
+++ list.php
@@ -1,5 +1,6 @@
 <?php
 require("config.php");
+//require("functions/logger.php");
 
 function parseToXML($htmlStr) 
 { 
@@ -12,10 +13,12 @@ return $xmlStr;
 } 
 
 // Opens a connection to a mySQL server
-$connection=mysql_connect (localhost, $username, $password);
+$connection=mysql_connect ("localhost", $username, $password);
 if (!$connection) {
   die('Not connected : ' . mysql_error());
 }
+
+//logToFile("log.log", "RADIO", "INFO", "Connect to DB");
 
 // Set the active mySQL database
 $db_selected = mysql_select_db($database, $connection);
@@ -24,8 +27,9 @@ if (!$db_selected) {
 }
 
 // Select all the rows in the markers table
-$query = "SELECT * FROM markers WHERE 1";
+$query = "SELECT * FROM markers WHERE band='LW'";
 $result = mysql_query($query);
+//logToFile("log.log", "RADIO", "INFO", "Select from  DB");
 if (!$result) {
   die('Invalid query: ' . mysql_error());
 }
@@ -52,8 +56,8 @@ if (!$result) {
     <th colspan="1"><a href="about.php">About</a></th>
     </tr>
     <tr>
-    <th>Long</th>
     <th>Lat</th>
+    <th>Long</th>
     <th>Name</th>
     <th>Address</th>
     <th colspan="2">Signal</th>
@@ -67,8 +71,8 @@ while ($row = @mysql_fetch_assoc($result)){
  // echo 'name="' . parseToXML('&','&amp;', $row['name']) . '" ';
  // echo 'address="' . parseToXML($row['address']) . '" ';
            echo "<tr>
-            <td>". $row['lng'] ." </td>
             <td>". $row['lat'] ." </td>
+            <td>". $row['lng'] ." </td>
             <td>". $row['name'] ." </td>
             <td>". $row['address'] ." </td>
             <td colspan=2>". $row['signal'] ." </td>
