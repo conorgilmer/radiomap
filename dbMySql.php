60b8992ab5b9bbbe4991296452bca5bec656cc1e
Conor Gilmer <conor.gilmer@gmail.com>
2016-07-12 09:26:05 +0100

    dbsql

--- dbMySql.php
+++ dbMySql.php
@@ -11,7 +11,9 @@ class DB_con
  
  public function insert($lng,$lat,$name,$address,$type,$freq,$band,$signal,$email)
  {
-  $res = mysql_query("INSERT markers(id, lat,lng,name,address,type, freq, band,signal, email, tstamp) VALUES('','$lat','$lng','$name','$address', '$type', '$freq', '$band' ,'$signal', '$email', now())");
+  $res = mysql_query("INSERT INTO `markers` (`id`, `name`, `address`, `lat`, `lng`, `rtype`, `freq`, `band`, `signal`, `email`, `tstamp`) VALUES (NULL, '$name','$address', '$lat', '$lng', '$type', '$freq', '$band', '$signal', '$email', now());");
+//  $res = mysql_query("INSERT into markers (id, lat, lng, name, address, rtype, freq, band,signal, email, tstamp) VALUES('','$lat','$lng','$name','$address', '$type', '$freq', '$band' ,'$signal', '$email', '')"); //now())");
+
   return $res;
  }
  
